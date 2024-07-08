11828d369ade4c01e996a08f06f0984438d1e6ac
Windows Box <windowsBox@sentracam.com>
2024-07-08 08:07:01 -0700

    Updated windows generated plugin files

---
+++ speech_to_text/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <speech_to_text/speech_to_text_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  SpeechToTextPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SpeechToTextPluginCApi"));
+}
